4313d561be4c9a593422d3fbf165e3ca47de6cc1
Alvaro Arturo <alvaroj.arturo@gmail.com>
2023-05-15 18:07:00 -0500

    Add state and useEffect to fetch Google API, render cards

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,13 +1,13 @@
-const Card = () => {
+const Card = (data) => {
   return (
-    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
-      <figure className='relative mb-3 w-full h-4/5'>
+    <div className='bg-white cursor-pointer w-56 h-60 rounded-lg mb-8'>
+      <figure className='relative mb-5 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/80 rounded-lg text-black text-xs m-2 p-1'>
-          Categorie
+          Categorie {data?.volumeInfo?.categories}
         </span>
         <img
           className='w-full h-full object-fit rounded-lg'
-          src='https://m.media-amazon.com/images/I/519OoDZ59fL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg'
+          src={data?.data?.volumeInfo?.imageLinks?.thumbnail}
           alt='book'
         />
         <div className='absolute top-0 right-0 flex justify-center items-center text-xs bg-white/80 w-6 h-6 rounded-full m-2'>
@@ -15,8 +15,10 @@ const Card = () => {
         </div>
       </figure>
       <p className='flex justify-between'>
-        <span className='text-sm font-light'>Book</span>
-        <span className='text-lg font-medium'>$0.00</span>
+        <span className='text-sm font-light'>
+          Author: {data?.data?.volumeInfo?.authors}
+        </span>
+        <span className='text-lg font-medium'>$3.00</span>
       </p>
     </div>
   )
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react'
 import Card from '../../Components/Card'
 import Layout from '../../Components/Layout'
 import '../App/App.css'
 
 function Home() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetch(
+      'https://www.googleapis.com/books/v1/volumes?q=Harry+potter&key=AIzaSyAKO_6L3sXSGn5H4NbsWss8w4Ym6kHmXug&langRestrict=es'
+    )
+      .then((response) => response.json())
+      .then((data) => setBooks(data.items))
+  }, [])
+
   return (
     <Layout>
-      <Card />
+      Home
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg '>
+        {books?.map((book) => (
+          <Card key={book.id} data={book} />
+        ))}
+      </div>
     </Layout>
   )
 }
