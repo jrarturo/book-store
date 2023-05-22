25649a03d76ad9b0889eeed76e8764231825d9d3
Alvaro Arturo <alvaroj.arturo@gmail.com>
2023-05-22 01:05:58 -0500

    Add some context data

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,8 +5,52 @@ export const BookShopContext = createContext()
 export const BookShopProvider = ({ children }) => {
   const [count, setCount] = useState(0)
 
+  const [isBookDetailOpen, setIsBookDetailOpen] = useState(false)
+  const openBookDetail = () => setIsBookDetailOpen(true)
+  const closeBookDetail = () => setIsBookDetailOpen(false)
+
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false)
+  const openCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(true)
+  const closeCheckoutSideMenu = () => setIsCheckoutSideMenuOpen(false)
+
+  const [bookDetail, setBookDetail] = useState({
+    volumeInfo: {
+      imageLinks: {
+        thumbnail: ''
+      },
+      title: '',
+      authors: '',
+      pageCount: ''
+    }
+  })
+
+  // add to cart
+  const [cartProducts, setCartProducts] = useState([])
+  const deleteBook = (id) => {
+    const newCartProducts = cartProducts.filter(
+      (product) => product.volumeInfo.id !== id
+    )
+    setCartProducts(newCartProducts)
+  }
+
   return (
-    <BookShopContext.Provider value={{ count, setCount }}>
+    <BookShopContext.Provider
+      value={{
+        count,
+        setCount,
+        openBookDetail,
+        closeBookDetail,
+        isBookDetailOpen,
+        bookDetail,
+        setBookDetail,
+        cartProducts,
+        setCartProducts,
+        isCheckoutSideMenuOpen,
+        openCheckoutSideMenu,
+        closeCheckoutSideMenu,
+        deleteBook
+      }}
+    >
       {children}
     </BookShopContext.Provider>
   )
